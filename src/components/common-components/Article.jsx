9ce3dbf8133e82d98f6d74dc72b4d1358cecaf8e
dbaric99 <damjana.baric1999@gmail.com>
2023-02-02 23:42:48 +0100

    Added some article items design

--- src/components/common-components/Article.jsx
+++ src/components/common-components/Article.jsx
@@ -1,35 +1,60 @@
-import React, { useEffect, useRef, useState } from 'react'
-import PropTypes from 'prop-types';
-import { generalHelper } from '../../helpers';
-import defaultImage from '../../assets/images/nytt.png'
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import { generalHelper } from "../../helpers";
+import defaultImage from "../../assets/images/nytt.png";
 
 function Article({ articleData }) {
+  const [mediaUrl, setMediaUrl] = useState();
+  const [date, setDate] = useState();
 
-    function handleArticleClick() {
-        window.open(articleData.url, '_blank', 'noreferrer');
-    }
+  function handleArticleClick() {
+    window.open(articleData.url, "_blank", "noreferrer");
+  }
+
+  useEffect(() => {
+    setMediaUrl(
+      articleData?.multimedia?.[1]?.url ||
+        articleData?.media?.[0]?.["media-metadata"]?.[1]?.url ||
+        defaultImage
+    );
+    setDate(generalHelper.formatDateToLocaleString(articleData.created_date));
+  }, []);
 
   return (
     <div className="article layout-spacing">
-        <aside className="article__image__wrapper">
-            <div className="article__image__holder" style={{backgroundImage: `url(${articleData?.multimedia?.[1]?.url || articleData?.media?.[0]?.["media-metadata"]?.[1]?.url || defaultImage})`, width: "200px", height:"100px"}}></div>
-            <span className="article__date">{ generalHelper.formatDateToLocaleString(articleData.created_date) }</span>
-            <span className="article__author">{ articleData?.byline }</span>
-        </aside>
-        <div className="article__content">
-            <p className="article__content__title" onClick={handleArticleClick()}>{ articleData?.title }</p>
-            <p className="article__content__description">{ articleData?.abstract }</p>
-        </div>
+      <aside className="article__image__wrapper">
+        <div
+          className="article__image__holder"
+          style={{
+            backgroundImage: `url(${mediaUrl})`,
+            width: "200px",
+            height: "100px",
+          }}
+        ></div>
+      </aside>
+      <div className="article__content">
+        <p className="article__content__title" onClick={handleArticleClick()}>
+          {articleData?.title}
+        </p>
+        <p className="article__content__description">{articleData?.abstract}</p>
+
+        <div className="article__image__details">
+        <span className="article__date">
+          {date !== 'Invalid Date' ? date : ''}
+        </span>
+        <span className="article__author">{articleData?.byline}</span>
+      </div>
+      </div>
     </div>
-  )
+  );
 }
 
 Article.propTypes = {
-    articleData: PropTypes.object
-}
+  articleData: PropTypes.object,
+};
 
 Article.defaultProps = {
-    articleData: ""
-}
+  articleData: "",
+};
 
 export { Article };
