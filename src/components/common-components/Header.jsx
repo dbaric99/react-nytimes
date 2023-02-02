4dbd81f3292b02b7d348cf6b5f66534dbf81c136
dbaric99 <damjana.baric1999@gmail.com>
2023-02-03 00:48:37 +0100

    Added possibility to toggle dark mode

--- src/components/common-components/Header.jsx
+++ src/components/common-components/Header.jsx
@@ -1,8 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import LogoSVGComponent from "./LogoSVGComponent";
 import sprite from "../../assets/images/sprite.svg";
 
 function Header() {
+
+  const [toggleDarkMode, setToggleDarkMode] = useState(false);
+
+  function toggleTheme() {
+    setToggleDarkMode(!toggleDarkMode);
+  }
 
   useEffect(()=>{
     var currentPage = window.location.pathname.slice(1);
@@ -11,9 +17,29 @@ function Header() {
     document.getElementById(elementId).classList.add('active');
   },[])
 
+  useEffect(() => {
+    if(toggleDarkMode) {
+      document.documentElement.style.setProperty('--main-white', '#343c44');
+      document.documentElement.style.setProperty('--main-black', '#f4f3ee');
+
+      document.documentElement.style.setProperty('--main-orange', '#2d93ad');
+      document.documentElement.style.setProperty('--main-blue', '#ce8147');
+
+      document.documentElement.style.setProperty('--background', '#000');
+    } else {
+      document.documentElement.style.setProperty('--main-black', '#343c44');
+      document.documentElement.style.setProperty('--main-white', '#f4f3ee');
+
+      document.documentElement.style.setProperty('--main-orange', '#ce8147');
+      document.documentElement.style.setProperty('--main-blue', '#2d93ad');
+
+      document.documentElement.style.setProperty('--background', '#fff');
+    }
+  }, [toggleDarkMode])
+
   return (
-    <div className="header layout-spacing">
-      <LogoSVGComponent className="header__logo" height="40px"></LogoSVGComponent>
+    <div className={`header layout-spacing ${toggleDarkMode ? 'dark' : ''}`}>
+      <LogoSVGComponent className='header__logo' height="40px" fill={toggleDarkMode ? "#f4f3ee" : ""}></LogoSVGComponent>
       <nav className="header__navbar__wrapper">
           <ul className="header__navbar">
             <li className="header__navbar__item">
@@ -33,8 +59,8 @@ function Header() {
             </li>
           </ul>
       </nav>
-      <svg className="header__theme__icon" height="20px" width="20px">
-        <use href={sprite + "#dark-mode-icon"} />
+      <svg className="header__theme__icon" height="20px" width="20px" onClick={toggleTheme}>
+        <use href={sprite + (toggleDarkMode ? "#bright-mode-icon" : "#dark-mode-icon")} />
       </svg>
     </div>
   );
